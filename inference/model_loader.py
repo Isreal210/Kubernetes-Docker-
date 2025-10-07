9a620a0d34b73d4c145830662230c1021628f822
Isreal210 <90050028+Isreal210@users.noreply.github.com>
2025-10-06 20:50:26 -0400

    Update model_loader.py

--- inference/model_loader.py
+++ inference/model_loader.py
@@ -1,1 +1,21 @@
+from inference.video_model import VideoDeepfakeModel
+
+class ModelContainer:
+    def __init__(self):
+        self.image = ImageDeepfakeModel()
+        self.audio = AudioDeepfakeModel()
+        self.text = TextDeepfakeModel()
+        self.video = VideoDeepfakeModel(self.image)
+
+    def predict(self, media_type, path_or_text):
+        if media_type == "image":
+            return self.image.predict(path_or_text)
+        elif media_type == "audio":
+            return self.audio.predict(path_or_text)
+        elif media_type == "text":
+            return self.text.predict(path_or_text)
+        elif media_type == "video":
+            return self.video.predict(path_or_text)
+        else:
+            raise ValueError("Unsupported media type")
 
